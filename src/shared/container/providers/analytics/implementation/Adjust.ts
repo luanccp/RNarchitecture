74a2cd1b1088967bb9f07d15ff8fe960b0f28c96
Luan Castheloge <luanccp@gmail.com>
2021-12-26 20:57:37 +0100

    fix resolve name

--- src/shared/container/providers/analytics/implementation/Adjust.ts
+++ src/shared/container/providers/analytics/implementation/Adjust.ts
@@ -3,8 +3,8 @@ import IAnalyticsProvider from '../model/IAnalyticsProvider';
 class AdjustProvider implements IAnalyticsProvider {
   private adjustClient = 'AdjustClient';
   sendEvent(params: string): Promise<string> {
-    return new Promise(respolve => {
-      return respolve + this.adjustClient + params;
+    return new Promise(resolve => {
+      return resolve + this.adjustClient + params;
     });
   }
 }
